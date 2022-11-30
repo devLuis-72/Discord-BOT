1fe96fe5751614ed7e268ec66605d8a93e44732b
Luis <luis.a.f.silverio@hotmail.com>
2022-11-30 03:12:53 -0300

    v1.0.0

---
+++ Cerberus/Cerberus/Comandos/ComandosBasicos.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerberus.Comandos
+{
+    public class ComandosBasicos : BaseCommandModule
+    {
+
+        [Command("avatar")]
+        [Description("Exibe o avatar de alguém.")]
+        [Aliases("av")]
+
+        public async Task Avatar(CommandContext ctx, [RemainingText] DiscordUser user = null)
+        {
+            await ctx.TriggerTypingAsync();
+            if (user == null) user = ctx.User;
+            await ctx.RespondAsync(user.AvatarUrl);
+        }
+
+
+        [Command("ping")]
+        [Description("Example ping command")]
+        [Aliases("pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":ping_pong:");
+
+            await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
+        }
+
+        [Command("alef")]
+        [Description("Example ping command")]
+        [Aliases("aleph")]
+        public async Task Alef(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":rainbow:");
+
+            await ctx.RespondAsync($"{emoji} FUCK ALEFÍ.");
+        }
+
+    }
+}
---
+++ Cerberus/Cerberus/Program.cs
@@ -0,0 +1,71 @@
+﻿using Cerberus.Comandos;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+
+namespace Cerberus
+{
+    public class Program
+    {
+        private DiscordClient _client;
+        static void Main(string[] args) => new Program().RunAsyncBot().GetAwaiter().GetResult();
+
+        public async Task RunAsyncBot()
+        {
+            DiscordConfiguration cfg = new DiscordConfiguration
+            {
+                Token = "",
+                TokenType = TokenType.Bot,
+                ReconnectIndefinitely = true,
+                GatewayCompressionLevel = GatewayCompressionLevel.Stream,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            _client = new DiscordClient(cfg);
+            _client.Ready += Client_Ready;
+            _client.ClientErrored += Client_ClientError;
+
+            string[] prefix = new string[1];
+            prefix[0] = "!";
+
+            CommandsNextExtension cnt = _client.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = prefix,
+                EnableDms = false,
+                CaseSensitive = false,
+                EnableDefaultHelp = false,
+                EnableMentionPrefix = true,
+                IgnoreExtraArguments = true
+
+            });
+            cnt.CommandExecuted += CntCommandExecuted;
+            cnt.RegisterCommands<ComandosBasicos>();
+
+            await _client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            sender.Logger.LogInformation("Cliente está pronto para processador os eventos.");
+            return Task.CompletedTask;
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            sender.Logger.LogError(e.Exception, "ERROR");
+            return Task.CompletedTask;
+        }
+
+        private Task CntCommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            e.Context.Client.Logger.LogInformation($"{e.Context.User.Username} executou com sucesso '{e.Command.QualifiedName}'");
+            return Task.CompletedTask;
+        }
+
+
+    }
+
+}
